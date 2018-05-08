e51be633d4043eb3241f4457e15eab4863242db3
prcela <kresimir.prcela@minus5.hr>
2018-05-08 16:05:37 +0200

    wip

--- bela/ViewControllers/RoomViewController.swift
+++ bela/ViewControllers/RoomViewController.swift
@@ -19,13 +19,54 @@ enum RoomSection
 class RoomViewController: UIViewController {
     
     let sections:[RoomSection] = [.Create,.FreeTables,.FreePlayers,.PlayingTables]
-
+    @IBOutlet weak var tableView: UITableView?
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        NotificationCenter.default.addObserver(self, selector: #selector(reload), name: Room.onInfo, object: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        WsAPI.shared.send(.RoomInfo)
     }
+    
+    func freePlayers() -> [PlayerInfo] {
+        return Room.shared.freePlayers().filter({ (pi) -> Bool in
+            return pi.id != PlayerStat.shared.id
+        })
+    }
+    
+    @objc func reload() {
+        tableView?.reloadData()
+    }
+    
+    @IBAction func back(_ sender: Any) {
+        navigationController?.popViewController(animated: true)
+    }
+    
+}
 
+extension RoomViewController: UITableViewDelegate
+{
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch sections[section] {
+        case .FreePlayers:
+            return lstr("Free players")
+        case .FreeTables:
+            return lstr("Free tables")
+        case .PlayingTables:
+            return lstr("Playing tables")
+        default:
+            return nil
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 }
 
 extension RoomViewController: UITableViewDataSource
@@ -34,10 +75,37 @@ extension RoomViewController: UITableViewDataSource
         return sections.count
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        switch sections[section] {
+        
+        case .Create:
+            let isFreePlayer = Room.shared.freePlayers().contains(where: { (playerInfo) -> Bool in
+                return playerInfo.id == PlayerStat.shared.id
+            })
+            return isFreePlayer ? 1:0
+            
+        case .FreePlayers:
+            return freePlayers().count
+        default:
+            return 0
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        switch sections[indexPath.section] {
+        case .Create:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "CellId")!
+            cell.textLabel?.text = lstr("Create new")
+            cell.accessoryType = .disclosureIndicator
+            return cell
+        case .FreePlayers:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "CellId")!
+            let player = freePlayers()[indexPath.row]
+            cell.textLabel?.text = player.alias
+            cell.accessoryType = .disclosureIndicator
+            return cell
+        default:
+            break
+        }
         return UITableViewCell(frame: .zero)
     }
     
