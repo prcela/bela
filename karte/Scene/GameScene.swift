cbfcb2977ef295f4a40b1d454b710993cc1c1d83
prcela <kresimir.prcela@minus5.hr>
2018-05-12 15:49:47 +0200

    wip

--- karte/Scene/GameScene.swift
+++ karte/Scene/GameScene.swift
@@ -28,15 +28,45 @@ class GameScene: SKScene {
             let cardW = size.width * 0.2
             let tex = SKTexture(imageNamed: card.imageName())
             let cardRatio = tex.size().width/tex.size().height
-            let cardNode = SKShapeNode(rectOf: CGSize(width: cardW, height: cardW/cardRatio), cornerRadius: cardW*0.08)
-            cardNode.fillColor = .white
-            cardNode.fillTexture = tex
-            cardNode.strokeColor = .black
+            let cardSize = CGSize(width: cardW, height: cardW/cardRatio)
+            let cardNode = SKCropNode()
+            let shapeNode = SKShapeNode(rectOf: cardSize, cornerRadius: cardW*0.08)
+            cardNode.maskNode = shapeNode
+            let spriteNode = SKSpriteNode(texture: tex, size: cardSize)
+            cardNode.addChild(spriteNode)
+            cardNode.name = "card"
+            shapeNode.fillColor = .white
+            shapeNode.strokeColor = .black
             cardNode.zPosition = 0.01*CGFloat(idx)
             cardNode.position = CGPoint(x: -150+CGFloat(idx)*40, y: -40+CGFloat(idx)*10)
             addChild(cardNode)
         }
         
         
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches {
+            self.touchUp(atPoint: t.location(in: self))
+        }
+    }
+    
+    fileprivate func touchUp(atPoint pos : CGPoint) {
+        var cards = [SKCropNode]()
+        enumerateChildNodes(withName: "card") { (card, _) in
+            cards.append(card as! SKCropNode)
+        }
+        // sort from top to bottom
+        cards.sort { (card0, card1) -> Bool in
+            return card0.zPosition > card1.zPosition
+        }
+        if let card = cards.first(where: { (card) -> Bool in
+            return card.contains(pos)
+        }) {
+            let action = SKAction.move(by: CGVector(dx: 10, dy: 50), duration: 0.2)
+            let action1 = SKAction.rotate(byAngle: CGFloat.pi, duration: 0.2)
+            card.run(action)
+            card.run(action1)
+        }
     }
 }
